fd132a3cfcd226dcf6814ba5f1925ccd61f80710
Tanvir Ahmed Talukder <rahidt78@gmail.com>
2023-11-28 11:36:03 +0600

    Date Counter project.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  return <Counter />;
+}
+
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  const date = new Date("Nov 22 2023");
+  date.setDate(date.getDate() + count);
+
+  function stepForward() {
+    return setStep((s) => s + 1);
+  }
+  function stepBackward() {
+    return setStep((s) => s - 1);
+  }
+  function countForward() {
+    return setCount((c) => c + step);
+  }
+  function countBackward() {
+    return setCount((c) => c - step);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="center">
+      <div>
+        <div>
+          <button onClick={stepBackward}> - </button>
+          <span>Step {step}</span>
+          <button onClick={stepForward}> + </button>
+        </div>
+        <div>
+          <button onClick={countBackward}>-</button>
+          <span>Count {count}</span>
+          <button onClick={countForward}>+</button>
+        </div>
+      </div>
+      <p>
+        <span>
+          {count === 0
+            ? "Today "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()} </span>
+      </p>
     </div>
   );
 }
